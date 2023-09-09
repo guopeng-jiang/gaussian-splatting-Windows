fddf64b4a452e76d95df48f7a46af6d07a97339d
Chris Cha <ccha@ucsd.edu>
2023-09-09 16:19:42 -0700

    handling new repo structure

---
+++ pinokio.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: "gaussian-splatting",
+      description: "3D Gaussian Splatting for Real-Time Radiance Field Rendering",
+      icon: "icon.png",
+  menu: [{
+    html: "<i class='fa-solid fa-plug'></i> Install",
+    type: "link",
+    href: "install_cuda.json?run=true&fullscreen=true"
+  },
+  {
+    html: "<i class='fa-solid fa-power-off'></i> Start",
+    type: "link",
+    href: "start.json?run=true&fullscreen=true"
+  }]
+}
